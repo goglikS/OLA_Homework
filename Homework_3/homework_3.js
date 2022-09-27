75d72019c94e302785de4aa808b8d701060b32cf
Giorgi <103255088+goglikS@users.noreply.github.com>
2022-09-27 11:05:59 +0400

    Add files via upload

---
+++ Homework_3/homework_3.js
@@ -0,0 +1,73 @@
+  const carInfo = {
+       getCarInfo(){
+        return this.make+ " "+this.model+" released in "+this.year;
+    },
+    
+    addOwner(owner){
+        this.owners.push(owner);
+    },
+    
+    removeOwner(owner){
+        var index = this.owners.indexOf(owner);
+             if (index > -1) { 
+             this.owners.splice(index, 1);
+             }
+         },
+         
+    getOwnersCount(){
+        return this.owners.length;
+    }  ,
+    
+    getOwnerNames(){
+    return this.owners.map((owner) => owner.fullName());
+     },
+    getFullInfo(){
+       return this.make+ " "+this.model+" from "+this.year+". "+this.getOwnersCount()+" person owns this car."+" These are - " + this.getOwnerNames()+ "."; 
+    }
+  };
+  
+function createCar(make,model,year,owners=[]) {
+  let car=Object.create(carInfo);
+  car.make = make;
+  car.model = model;
+  car.year=year;
+  car.owners=owners;
+
+  return car;
+}
+
+
+
+function createPerson(name, surname, age, gender, cars = []) {
+  let person = Object.create(personInfo);
+  person.name=name;
+  person.surname=surname;
+  person.age =age;
+  person.gender = gender;
+  person.cars= cars;
+
+  return person;
+}
+  const personInfo = {
+     fullName() {
+    return this.name+" "+this.surname;
+        },
+    countCars(){
+        return this.cars.length;
+    },
+    buysCar(car){
+         this.cars.push(car);
+         car.addOwner(this);
+    },
+    sellsCar(car){
+         
+         var index = this.cars.indexOf(car);
+             if (index > -1) { 
+             this.cars.splice(index, 1);
+             }
+          car.removeOwner(this);
+    },
+    getAllCarsInfo(){
+        return this.name+" owns these cars: "+this.cars.map((car) => car.getCarInfo());
+    }
+  }
