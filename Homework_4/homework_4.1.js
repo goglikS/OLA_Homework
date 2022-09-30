a7d3cbed75d325e432972b2246b714e5b5570c31
Giorgi <103255088+goglikS@users.noreply.github.com>
2022-09-30 19:49:46 +0400

    Add files via upload

---
+++ Homework_4/homework_4.1.js
@@ -0,0 +1,42 @@
+console.log("Start");
+
+const usersDB = {
+  "user1@hw.js": [{ title: "video1" }, { title: "video2" }],
+  "user2@hw.js": [{ genre: "genre1" }, { genre: "genre1" }],
+  "user3@hw.js": [],
+};
+
+function loginUser(email, password, callback) {
+  setTimeout(() => {
+    console.log("Now we have the data of user: ", email);
+    callback({ userEmail: email });
+  }, 0);
+}
+
+function getUserVideos(email, callback) {
+  setTimeout(() => {
+    callback(usersDB[email]);
+  }, 0);
+}
+
+function videoDetails(video, callback) {
+  setTimeout(() => {
+    callback(video.title);
+  }, 0);
+}
+const getPassedUsersFirstVideoTitle = (user) =>
+  loginUser("user1@hw.js", 1234, (user) => {
+    console.log("user : ", user);
+    getUserVideos(user.userEmail, (videos) => {
+      console.log("videos :", "(" + videos.length + ")");
+      videos.forEach(function (item, index, array) {
+        console.log(index + ": ", item);
+      });
+      videoDetails(videos[0], (title) => {
+        console.log("title of first video is :", title);
+      });
+    });
+  });
+
+getPassedUsersFirstVideoTitle("user1@hw.js");
+console.log("Finish");
